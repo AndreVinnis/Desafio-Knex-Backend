9a95ced7571a58666ee2f4d56def9b797bb3d44c
AndreVinnis <andrev.macambira@gmail.com>
2026-07-18 10:53:25 -0300

    perf: ajustando dtos para validar entrada

--- src/main/java/com/uepb/DesafioKnex/dto/request/UserRequest.java
+++ src/main/java/com/uepb/DesafioKnex/dto/request/UserRequest.java
@@ -1,11 +1,17 @@
 package com.uepb.DesafioKnex.dto.request;
 
 import com.uepb.DesafioKnex.model.enums.Role;
+import jakarta.validation.constraints.NotNull;
 
 public record UserRequest(
+
+        @NotNull
         String name,
+        @NotNull
         String email,
+        @NotNull
         String password,
+        @NotNull
         Role role
 ) {
 }
